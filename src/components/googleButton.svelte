b0a6a809af93a48981d9da3a59beaa8df19e10d5
arafatiqbal <arafatiqbal123@gmail.com>
2023-10-20 23:53:09 -0700

    Add google auth - doesn't work yet

--- src/components/googleButton.svelte
+++ src/components/googleButton.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <button
-	on:click={() => signIn('github', { callbackUrl: '/dashboard' })}
+	on:click={() => signIn('google', { callbackUrl: '/dashboard' })}
 	type="button"
 	class="float-right w-max text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mb-2"
 >
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,24 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
-import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import GoogleProvider from '@auth/core/providers/google';
+import {
+	GITHUB_ID,
+	GITHUB_SECRET,
+	GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET
+} from '$env/static/private';
 import type { Provider } from '@auth/core/providers';
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 
+const githubProvider = GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }) as Provider;
+const googleProvider = GoogleProvider({
+	clientId: GOOGLE_CLIENT_ID,
+	clientSecret: GOOGLE_CLIENT_SECRET
+}) as Provider;
+
 export const handle: Handle = sequence(
 	SvelteKitAuth({
-		providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET }) as Provider]
+		providers: [githubProvider, googleProvider]
 	})
 );
