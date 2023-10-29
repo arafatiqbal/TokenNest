c60f4a9da0a1692b02d105708bc9e4a61c61fa95
arafatiqbal <arafatiqbal123@gmail.com>
2023-10-28 23:00:42 -0700

    Update the arrangement of items

--- src/components/dataTable.svelte
+++ src/components/dataTable.svelte
@@ -6,30 +6,35 @@
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg mx-10">
   <div class="bg-white m-4">
     <label for="table-search" class="sr-only">Search</label>
-    <div class="relative mt-1">
-      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <svg
-          class="w-4 h-4 text-gray-500"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 20 20"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-          />
-        </svg>
+    <div class="flex justify-between mx-auto p-4">
+      <h1 class="px-5 text-2xl font-bold leading-none tracking-tight text-gray-900 md:text-3xl">
+        Dashboard
+      </h1>
+      <div class="relative mt-1">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            class="w-4 h-4 text-gray-500"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="table-search"
+          class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+          placeholder="Search for items"
+        />
       </div>
-      <input
-        type="text"
-        id="table-search"
-        class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
-        placeholder="Search for items"
-      />
     </div>
   </div>
   <table class="w-full text-sm text-left text-gray-500">
